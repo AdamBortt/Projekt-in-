b5ff712a4d080e1b64c8579a7f3570520c59e1ff
AdamBort <bortkun62@gmail.com>
2022-12-14 01:02:46 +0100

    removed avatars routing path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { InitialsFormComponent } from './components/initials-form/initials-form.component';
-import { AvatarsFormComponent } from './components/avatars-form/avatars-form.component';
 import { CustomFormComponent } from './components/custom-form/custom-form.component';
 
 const routes: Routes = [
-  {
-    path: 'avatars',
-    component: AvatarsFormComponent
-  },
   {
     path: 'template',
     component: InitialsFormComponent
