5516b1391dcecbb11af4b87268c38c0778e6e271
AdamBort <bortkun62@gmail.com>
2022-12-28 23:25:31 +0100

    Added .svg pictures

--- src/app/services/templates-service.service.ts
+++ src/app/services/templates-service.service.ts
@@ -1,19 +1,19 @@
 export class TemplatesService {
-  head = '../assets/templates/Head.png'
+  head = '../assets/templates/Head.svg'
   
   bodies = [
-    '../../../../assets/templates/Bodies/Body1.png',
-    '../../../../assets/templates/Bodies/Body2.png',
-    '../../../../assets/templates/Bodies/Body3.png',
-    '../../../../assets/templates/Bodies/Body4.png',
-    '../../../../assets/templates/Bodies/Body5.png',
+    '../../../../assets/templates/Bodies/Body1.svg',
+    '../../../../assets/templates/Bodies/Body2.svg',
+    '../../../../assets/templates/Bodies/Body3.svg',
+    '../../../../assets/templates/Bodies/Body4.svg',
+    '../../../../assets/templates/Bodies/Body5.svg',
   ]
 
   hairs = [
-    '../../../../assets/templates/Hairs/Hair1.png',
-    '../../../../assets/templates/Hairs/Hair2.png',
-    '../../../../assets/templates/Hairs/Hair3.png',
-    '../../../../assets/templates/Hairs/Hair4.png',
-    '../../../../assets/templates/Hairs/Hair5.png',
+    '../../../../assets/templates/Hairs/Hair1.svg',
+    '../../../../assets/templates/Hairs/Hair2.svg',
+    '../../../../assets/templates/Hairs/Hair3.svg',
+    '../../../../assets/templates/Hairs/Hair4.svg',
+    '../../../../assets/templates/Hairs/Hair5.svg',
   ]
 }
