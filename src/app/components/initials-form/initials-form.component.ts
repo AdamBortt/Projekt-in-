1a507d67bc1dde2a88d372fcbe35217c51360420
AdamBort <bortkun62@gmail.com>
2022-12-30 20:58:21 +0100

    moved functions to service

--- src/app/components/initials-form/initials-form.component.ts
+++ src/app/components/initials-form/initials-form.component.ts
@@ -10,16 +10,10 @@ export class InitialsFormComponent implements OnInit{
 
   constructor(public service: service) {  }
 
-  r: number;
-  g: number;
-  b: number;
-  backLightness: number = 1;
-  fontLightness: number = 0;
-  warning: boolean = false;
   showCharacterSelect: boolean = false;
   
   ngOnInit(): void {
-    this.checkValue();
+    this.service.checkValue();
     this.service.selectedIcon = null;
   }
 
@@ -37,70 +31,6 @@ export class InitialsFormComponent implements OnInit{
     else 
     {
       this.service.initialsSize = 225 - this.service.imgSize;
-    }
-  }
-
-  nameChange(event: any) {
-    this.service.firstNameLetter = event.target.value.charAt(0).toUpperCase();
-    console.log('name is: ', this.service.firstNameLetter);
-    this.checkValue();
-  }
-
-  lastNameChange(event: any) {
-    this.service.firstLastNameLetter = event.target.value.charAt(0).toUpperCase();
-    console.log('name is: ', this.service.firstLastNameLetter);
-    this.checkValue();
-  }
-
-  checkValue() {
-    if (this.service.firstNameLetter == "" && this.service.firstLastNameLetter == "")
-    this.service.downloadButtonEnabled = true;
-    else
-    this.service.downloadButtonEnabled = false;
-  }
-
-  colorChange(event: any) {
-    this.hexToRgb(event.target.value);
-    document.getElementById('avatar').style.backgroundColor = `rgb(${this.r}, ${this.g}, ${this.b})`;
-    this.backLightness = ((this.r * 0.2126) + (this.g * 0.7152) + (this.b * 0.0722)) / 255;
-    this.fontCalculate()
-  }
-  
-  fontCalculate() {
-   console.log("backLightness: " + this.backLightness)
-   console.log("fontLightness: " + this.fontLightness)
-   var diff = this.backLightness - this.fontLightness;
-   if ( diff >= -0.15 && diff <= 0.15 ) {
-     console.log("font ledwo widoczny!")
-     this.warning = true;
-   }
-   else
-   {
-     this.warning = false;
-   }
-  }
-
-  hexToRgb(hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    this.r = parseInt(result[1], 16)
-    this.g = parseInt(result[2], 16)
-    this.b = parseInt(result[3], 16)
-    console.log(this.r, this.g, this.b);
-  }
-
-  fontColorChange(event: any) {
-    this.hexToRgb(event.target.value)
-    this.fontLightness = ((this.r * 0.2126) + (this.g * 0.7152) + (this.b * 0.0722)) / 255;
-    this.fontCalculate();
-    document.getElementById('initials').style.color = event.target.value;
-  }
-
-  squareChange(event: boolean) {
-    if (event == true) {
-      document.getElementById('avatar').style.borderRadius = "0px"; 
-    }
-    else {
-      document.getElementById('avatar').style.borderRadius = "100%";
     }
   }
 }
--- src/app/services/service.ts
+++ src/app/services/service.ts
@@ -6,6 +6,13 @@ export class service {
   firstLastNameLetter: string = "";
   selectedIcon: string = "";
 
+  backLightness: number = 1;
+  fontLightness: number = 0;
+  r: number;
+  g: number;
+  b: number;
+  warning: boolean = false;
+
   imgSize: number = 175;
   initialsSize: number = 150;
   imageUrl: string;
@@ -18,5 +25,69 @@ export class service {
     this.firstNameLetter=""
     this.firstLastNameLetter=""
     this.selectorButtonEnabled = true
+  }
+
+  nameChange(event: any) {
+    this.firstNameLetter = event.target.value.charAt(0).toUpperCase();
+    console.log('name is: ', this.firstNameLetter);
+    this.checkValue();
+  }
+
+  lastNameChange(event: any) {
+    this.firstLastNameLetter = event.target.value.charAt(0).toUpperCase();
+    console.log('name is: ', this.firstLastNameLetter);
+    this.checkValue();
+  }
+
+  checkValue() {
+    if (this.firstNameLetter == "" && this.firstLastNameLetter == "")
+    this.downloadButtonEnabled = true;
+    else
+    this.downloadButtonEnabled = false;
+  }
+
+  colorChange(event: any) {
+    this.hexToRgb(event.target.value);
+    document.getElementById('avatar').style.backgroundColor = `rgb(${this.r}, ${this.g}, ${this.b})`;
+    this.backLightness = ((this.r * 0.2126) + (this.g * 0.7152) + (this.b * 0.0722)) / 255;
+    this.fontCalculate()
+  }
+
+  fontCalculate() {
+    console.log("backLightness: " + this.backLightness)
+    console.log("fontLightness: " + this.fontLightness)
+    var diff = this.backLightness - this.fontLightness;
+    if ( diff >= -0.15 && diff <= 0.15 ) {
+      console.log("font ledwo widoczny!")
+      this.warning = true;
+    }
+    else
+    {
+      this.warning = false;
+    }
+   }
+   
+   hexToRgb(hex) {
+     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+     this.r = parseInt(result[1], 16)
+     this.g = parseInt(result[2], 16)
+     this.b = parseInt(result[3], 16)
+     console.log(this.r, this.g, this.b);
+   }
+
+   fontColorChange(event: any) {
+    this.hexToRgb(event.target.value)
+    this.fontLightness = ((this.r * 0.2126) + (this.g * 0.7152) + (this.b * 0.0722)) / 255;
+    this.fontCalculate();
+    document.getElementById('initials').style.color = event.target.value;
+  }
+
+  squareChange(event: boolean) {
+    if (event == true) {
+      document.getElementById('avatar').style.borderRadius = "0px"; 
+    }
+    else {
+      document.getElementById('avatar').style.borderRadius = "100%";
+    }
   }
 }
