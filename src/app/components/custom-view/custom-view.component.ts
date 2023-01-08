c920a566449c0b315a559928308b9b6a521b6ac2
AdamBort <bortkun62@gmail.com>
2023-01-09 00:48:08 +0100

    Added wrong format alert

--- src/app/components/custom-view/custom-view.component.ts
+++ src/app/components/custom-view/custom-view.component.ts
@@ -8,18 +8,28 @@ import { DataService } from '../../services/Data.service';
 })
 export class CustomViewComponent {
 
+  allowedExtentions = ['jpg', 'png', 'jpeg', 'svg', 'webp']
+
   constructor(public DataService: DataService) {  }
 
   onFileChange(event) {
     const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (e: any) => {
-      this.DataService.imageUrl = e.target.result;
-    };
-    reader.readAsDataURL(file);
-    this.DataService.initialsSize = 225 - this.DataService.imgSize
-    this.DataService.downloadButtonEnabled = false
-    this.DataService.imageDisplay = true
+    const fileCheck = event.target.files
+    let fileExtention = fileCheck[0].name.split('.').pop();
+    if (!this.allowedExtentions.includes(fileExtention))
+    {
+      alert('Wrong file format ' + fileExtention)
+    }
+    else {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.DataService.imageUrl = e.target.result;
+      };
+      reader.readAsDataURL(file);
+      this.DataService.initialsSize = 225 - this.DataService.imgSize
+      this.DataService.downloadButtonEnabled = false
+      this.DataService.imageDisplay = true
+      }
   }
 
 }
